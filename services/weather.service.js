0858cca2e0328f85f5c56d31d6968470c6da238b
Jesus Saul Hernandez <jesus.castaneda@metricamovil.com>
2021-10-25 23:28:12 -0500

    Del logs

--- services/weather.service.js
+++ services/weather.service.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 const getWeather = async(cityId, start) => {
     try {
         const resp = await axios.get( `${process.env.OPW_DIR}id=${cityId}&type=hour&appid=${process.env.API_KEY_OP_1}&start=${start}&cnt=24`);
-        console.log('RESP', resp);
         return resp.data.list;       
     } catch (error) {
         try {
